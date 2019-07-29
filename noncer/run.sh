6cf88dddf2837e2f5247735c7713547f46dd90ab
Supichai-ss <33937135+Supichai-ss@users.noreply.github.com>
2019-07-30 06:27:28 +0700

    Update run.sh

--- noncer/run.sh
+++ noncer/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./noncerpro --address='NQ69 84KV 0998 6RV5 FBVH 2SY8 HSK4 9PDM RQQ3' --name=PK-P-C-01-Scaleway --server=nimiq.icemining.ca --port=2053 --mode=dumb
+./noncerpro --address='NQ69 84KV 0998 6RV5 FBVH 2SY8 HSK4 9PDM RQQ3' --name=PK-P-D-01-Scaleway --server=nimiq.icemining.ca --port=2053 --mode=dumb
